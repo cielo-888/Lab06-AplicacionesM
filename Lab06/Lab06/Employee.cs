2359051710a372cae546bb1fd4de578bd9c2341e
cielo <cielo.espinoza@tecsup.edu.pe>
2022-04-29 16:55:32 -0500

    Agregar archivos de proyecto.

---
+++ Lab06/Lab06/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Lab06
+{
+   public class Employee
+    {
+        public string DisplayName { get; set; }
+
+    }
+}
